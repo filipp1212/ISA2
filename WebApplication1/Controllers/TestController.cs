fae339ebfe223d4e11519005a952cfec7cbf1b0b
Filipp Djatšuk <fidjat@ttu.ee>
2019-02-20 13:39:12 +0200

    MVC2

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Labor1.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello world";
+        }
+
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+    }
+
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Salary { get; set; }
+    }
+}
